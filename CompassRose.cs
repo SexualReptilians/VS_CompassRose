6b33c2078cadb4c5860b27d19f35487631a0572e
meganukebmp <kristiana2000@abv.bg>
2023-06-04 01:57:14 +0200

    Initial Commit

---
+++ CompassRose.cs
@@ -0,0 +1,22 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace CompassRose
+{
+    public class CompassRose : ModSystem
+    {
+        ICoreClientAPI capi;
+
+        public override bool ShouldLoad(EnumAppSide forSide)
+        {
+            return forSide == EnumAppSide.Client;
+        }
+        
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+
+            capi = api;
+        }
+    }
+}
